d2eb6ad11095ca6a6306619bda6dd19714a8dfe0
Artyom <itcoding76@gmail.com>
2023-04-20 20:50:28 +0200

    Home Work 3 is done,

--- HW3/main.py
+++ HW3/main.py
@@ -1,9 +1,10 @@
 from HW3.task1 import task1_start
 from HW3.task2 import task2_start
+from HW3.task2 import task3
 
 if __name__ == '__main__':
     # task1_start()
     # task2_start()
-    task2_start()
+    task3()
 
 
